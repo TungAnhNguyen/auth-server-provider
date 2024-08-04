bfa84915c30c402139e51db3fc19a3405e3a86cd
nguyentunganh <nguyentunganh96hn@gmail.com>
2024-08-04 18:28:42 +0700

    first commit: auth server provider and mock server requesting authen.

---
+++ auth-client/src/test/java/com/authserver/tunganh/AuthClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authserver.tunganh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
